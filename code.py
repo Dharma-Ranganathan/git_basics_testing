e9e75583e3ada0afc74ded9d736a45775fe1061e
Dharma-Ranganathan <dharmaqubla369@gmail.com>
2025-12-05 12:01:39 +0530

    updated code.py file with python func to check balance in bank

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
-def convey(msg):
-    print(msg)
+print("Check my balance")
 
-convey('my greetings to you...')
+balance = 1000
+
+def check_balance():
+    print(f'My available balance is {balance}')
+
+check_balance()
